25fdb0a2844e632f48e6519cb6253af4a8554d20
anthony <anthonymolinari>
2023-03-09 16:19:19 -0800

    ci/cd setup

--- src/main.py
+++ src/main.py
@@ -1,4 +1,20 @@
+import sys
+from datetime import datetime
 
+#
+# 
+def parseConfig():
+    print(f'{datetime.now()}: loading config')
+
+
+def main(): 
+    # load configuration
+    parseConfig()
+
+    # check for config file
+    # validate config
+
+    # create threads for each container
 
 
 if __name__ == '__main__':
