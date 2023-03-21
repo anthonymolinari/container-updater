b10d8c377d0fff2f6d26b35871ac65f2499798f6
anthony <anthonymolinari>
2023-03-21 10:59:06 -0700

    ci changes

--- dagger/pipeline.py
+++ dagger/pipeline.py
@@ -5,24 +5,16 @@ import dagger
 async def buildImage():
      async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
           src = client.host().directory(".")
-          # username = client.host().env_variable("DOCKER_HUB_USERNAME")
-          token = client.host().env_variable("DOCKER_HUB_TOKEN")
-
           python = (
                client.container()
                .with_mounted_directory("/app", src)
                .with_workdir("/app")
                .build(src)
-               .with_registry_auth(
-                    "docker.io",
-                    "anthonymolinari",
-                    token.secret()
-               )
                .publish("docker.io/anthonymolinari/container-updater:latest")
           )
           await python
 
-     print("Finished build")
+     print("Pipeline Done")
 
 
 if __name__ == "__main__":
